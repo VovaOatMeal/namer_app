981a694dbadba229abc12f464ad80f10d1d9e6ac
VovaOatMeal <shypik555@gmail.com>
2023-01-07 01:34:37 +0200

    first commit

---
+++ lib/routes/favorites_page.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../main.dart';
+
+class FavoritesPage extends StatefulWidget {
+  const FavoritesPage({Key? key}) : super(key: key);
+
+  @override
+  State<FavoritesPage> createState() => _FavoritesPageState();
+}
+
+class _FavoritesPageState extends State<FavoritesPage> {
+  void removeFavorite(MyAppState appState, int index) {
+    var favorites = appState.favorites;
+    setState(() {
+      favorites.removeAt(index);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var favorites = appState.favorites;
+
+    return favorites.isEmpty
+        ? Center(child: Text("You haven't liked any phrases yet."))
+        : Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 24.0, vertical: 16.0),
+                child: Text("You have ${favorites.length} favorites:"),
+              ),
+              ListView.builder(
+                itemCount: favorites.length,
+                shrinkWrap: true,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    leading: IconButton(
+                      onPressed: () => removeFavorite(appState, index),
+                      icon: Icon(Icons.favorite),
+                    ),
+                    title: Text(appState.favorites[index].asPascalCase),
+                  );
+                },
+              ),
+            ],
+          );
+  }
+}
